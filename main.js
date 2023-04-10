1bb824cc79b65866932e6b3459101872e84d865d
visitorred <reduteklay1479@gmail.com>
2023-04-10 22:04:13 +0300

    initial commit.

---
+++ main.js
@@ -0,0 +1,42 @@
+/* document.getElementById("btn").onclick=function () {
+    document.getElementById("confirm").innerHTML="order palced , check your email!";
+    document.getElementById("btn").style.display="none"
+} */
+
+/* var username=prompt();
+console.log(username) */
+/* (function () {
+    //IIFE -immediately invoked function expression
+var age=5;  
+
+    
+})(); */
+
+  /* let person={
+    name:"Visitor",
+    age:20,
+    favFood:"shro...obviously",
+    fun:function(){
+      console.log("yay!")
+    }
+
+  }
+  person.fun();
+  console.log(person);
+     */
+     
+/*   let myName="visitor";
+ myName= myName.toUpperCase();
+
+  let yourName=new String("Outsider");
+  console.log(myName);
+  console.log(typeof(myName));
+  console.log(typeof(yourName)); */
+
+  /*var input=prompt("Put in a number");
+  console.log("Decimal :",input);
+  console.log("Binary :",Number.parseInt(input,2));  */ 
+  {
+    var input=Number(prompt("put in a decimal number"));
+    console.log(input," in decimal to binary",input.toString(2));
+  }
